a3483d3b97a55a1c29e359bf5eef8d1222a21d96
Pooja <poojasinghpos@gmail.com>
2024-03-08 15:57:38 -0800

    DP-1 Submitted

---
+++ Coin_Change.py
@@ -0,0 +1,14 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> inf:
+        res = [float('inf')]*(amount+1)
+        res[0] = 0
+        for i, v in enumerate(res):
+            for coin in coins:
+                if i>=coin:
+                    res[i] = min(res[i], res[i-coin]+1)
+        return res[-1] if res[-1]!=float('inf') else -1
+s = Solution()
+print(s.coinChange([1,2,5], 11))
+print(s.coinChange([2], 3))
+print(s.coinChange([1], 0))
+print(s.coinChange([3,7,405,436], 8839))
